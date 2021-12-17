538a50182eb290bf97c9c0a78de2d715322e6f05
Anna Prutnik <prutnik.anna@gmail.com>
2021-12-17 15:28:40 +0200

    resolve hw-02

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,56 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+} = require('../../model/');
+
+const {
+  addContactValidation,
+  changeContactValidation,
+} = require('../../middleware/validationMiddleware');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const contacts = await listContacts();
+  res.status(200).json({contacts});
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const contact = await getContactById(id);
+  if (contact) {
+    return res.status(200).json({contact});
+  }
+  return res.status(404).json({message: 'Not found'});
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', addContactValidation, async (req, res, next) => {
+  const body = req.body;
+  const newContact = await addContact(body);
+  res.status(201).json({newContact});
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const deleteStatus = await removeContact(id);
+  if (!deleteStatus) {
+    return res.status(404).json({message: 'Not found'});
+  }
+  res.status(200).json({message: 'contact deleted'});
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put('/:id', changeContactValidation, async (req, res, next) => {
+  const id = req.params.id;
+  const body = req.body;
+  const changeContact = await updateContact(id, body);
+  if (!changeContact) {
+    return res.status(404).json({message: 'Not found'});
+  }
+  res.status(200).json({changeContact});
+});
 
-module.exports = router
+module.exports = router;
